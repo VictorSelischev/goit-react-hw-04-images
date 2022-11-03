44bfa447a3f7f97037581d619f4cc783c8dd8c29
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-03 13:21:00 +0200

    Refactoring component Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,16 @@
-import { Component } from 'react';
+import React from 'react';
 import css from './Button.module.css';
 import { PropTypes } from 'prop-types';
 
-class Button extends Component {
-  render() {
-    return (
-      <>
-        <button
-          className={css.buttonGallery}
-          type="button"
-          onClick={this.props.onClick}
-        >
-          Load more
-        </button>
-      </>
-    );
-  }
-}
-
-export { Button };
+export const Button = ({ onClick }) => {
+  return (
+    <>
+      <button className={css.buttonGallery} type="button" onClick={onClick}>
+        Load more
+      </button>
+    </>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
